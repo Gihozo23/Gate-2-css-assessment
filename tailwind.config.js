684f528c9ee8ad121725a1b748c6378232026001
Gihozo23 <christellegihozo23@gmail.com>
2024-05-17 12:24:34 +0200

    second section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily:{
         lato: ['Lato']
       },
+      colors:{
+        customGold: '#a18a4d',
+        myColor:"#333",
+      },
     },
   },
   plugins: [],
